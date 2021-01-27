dba6f37c165af7d05c4a93dbcee27810dab1e1bc
Azambik <71612017+Azambik@users.noreply.github.com>
2021-01-27 17:16:19 -0500

    added routes for making post

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require("./User");
 const Post = require("./Post");
+const { post } = require("../routes/api/user-routes");
+
+//create associations
+User.hasMany(Post, {
+    foreignKey: "user_is'"
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 module.exports = { User, Post };
